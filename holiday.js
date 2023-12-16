b4c6bd15839dbe6745d3dd26ced393cca0d95c26
1229mayumi <letrico1229@yahoo.co.jp>
2023-12-16 13:28:59 +0900

    kadai_009

---
+++ holiday.js
@@ -0,0 +1,14 @@
+//holidaysを宣言
+const holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"]
+
+//for を使用してリストの中身を出力
+for (let holiday of holidays){
+ console.log(holiday);
+}
+
+//while を使用してリストの中身を出力
+let i = 0;
+while (i < holidays.length){
+  console.log(holidays[i]);
+  i++;
+}
